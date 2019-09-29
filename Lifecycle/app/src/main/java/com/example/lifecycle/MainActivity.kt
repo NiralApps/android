a291661307c4eeddb54dc32e6eda89e804b5281c
NiralSundar <sundar.raj37@gmail.com>
2019-09-29 23:53:55 +0530

    life cycle added

--- Lifecycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ Lifecycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this@MainActivity, "Created", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this@MainActivity, "Started", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this@MainActivity, "Paused", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this@MainActivity, "Resumed", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this@MainActivity, "Stopped", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this@MainActivity, "Restarted", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this@MainActivity, "Destroyed", Toast.LENGTH_SHORT).show()
     }
 }
