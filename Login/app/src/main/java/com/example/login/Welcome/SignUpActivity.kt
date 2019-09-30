7671685f65ded2a0d09d426ede86b49679cba013
NiralSundar <sundar.raj37@gmail.com>
2019-10-01 01:09:32 +0530

    Sign Up Screen UI Completed

---
+++ Login/app/src/main/java/com/example/login/Welcome/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.example.login.Welcome
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.login.R
+
+class SignUpActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
