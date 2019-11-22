bce597a22515e2b34919293d23e167a987e74852
NiralSundar <sundar.raj37@gmail.com>
2019-11-22 23:29:49 +0530

    My Account screen

--- FarmerApp/app/src/main/java/com/example/farmerapp/ui/HomeFragment.java
+++ FarmerApp/app/src/main/java/com/example/farmerapp/ui/HomeFragment.java
@@ -1,33 +1,96 @@
 package com.example.farmerapp.ui;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.farmerapp.R;
+import com.example.farmerapp.ViewPagerAdapter;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
-
+    ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
+    List<Integer> color;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_home_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_menu, container, false);
+        final Context context = getContext();
+        final Activity activity = getActivity();
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        CardView cardView3 = view.findViewById(R.id.cardThree);
+        sliderDotspanel = (LinearLayout) view.findViewById(R.id.SliderDots);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(context);
+        viewPager.setAdapter(viewPagerAdapter);
+        dotscount = viewPagerAdapter.getCount();
+        dots = new ImageView[dotscount];
+        for(int i = 0; i < dotscount; i++){
+            dots[i] = new ImageView(context);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(context, R.drawable.nonactive_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 8, 0);
+            sliderDotspanel.addView(dots[i], params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(context, R.drawable.active_dot));
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(activity.getApplicationContext(), R.drawable.nonactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(activity.getApplicationContext(), R.drawable.active_dot));
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+//        Timer timer = new Timer();
+//        timer.scheduleAtFixedRate(new SliderTimer(), 4000, 6000);
+        return view;
     }
-
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Menu 1");
     }
+//    private class SliderTimer extends TimerTask {
+//        @Override
+//        public void run() {
+//            getActivity().runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    if (viewPager.getCurrentItem() < 3) {
+//                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+//                    } else {
+//                        viewPager.setCurrentItem(0);
+//                    }
+//                }
+//            });
+//        }
+//    }
 }
