686f739af13f3b29c12a446e1afe0595dcc4c71e
NiralSundar <sundar.raj37@gmail.com>
2019-11-24 23:03:42 +0530

    Issues fixed, Changes done

--- FarmerApp/app/src/main/java/com/example/farmerapp/HomeActivity.java
+++ FarmerApp/app/src/main/java/com/example/farmerapp/HomeActivity.java
@@ -1,22 +1,31 @@
 package com.example.farmerapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.example.farmerapp.ui.CategoriesFragment;
 import com.example.farmerapp.ui.CustomerCareFragment;
 import com.example.farmerapp.ui.FAQFragment;
+import com.example.farmerapp.ui.FrequentlyBoughtFragment;
 import com.example.farmerapp.ui.HomeFragment;
+import com.example.farmerapp.ui.ManageAddressFragment;
 import com.example.farmerapp.ui.MyAccountFragment;
+import com.example.farmerapp.ui.MyOrdersFragment;
+import com.example.farmerapp.ui.MyStoreFragment;
 import com.example.farmerapp.ui.NotificationFragment;
 
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.ui.AppBarConfiguration;
 
+import com.example.farmerapp.ui.PaymentOptionsFragment;
+import com.example.farmerapp.ui.ReferAndEarnFragment;
+import com.example.farmerapp.ui.WishListFragment;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -25,16 +34,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener  {
     private AppBarConfiguration mAppBarConfiguration;
+    public static ImageView notificationIcon;
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        notificationIcon = findViewById(R.id.notificationIcon);
+        builder = new AlertDialog.Builder(this);
+        notificationIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySelectedScreen(R.id.notificationIcon);
+            }
+        });
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -67,8 +89,12 @@ public class HomeActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_my_orders) {
+            displaySelectedScreen(R.id.action_my_orders);
+        } else if (id == R.id.action_wishlist) {
+            displaySelectedScreen(R.id.action_wishlist);
+        } else if (id == R.id.action_my_store) {
+            displaySelectedScreen(R.id.action_my_store);
         }
         return super.onOptionsItemSelected(item);
     }
@@ -86,8 +112,17 @@ public class HomeActivity extends AppCompatActivity
             case R.id.nav_categories:
                 fragment = new CategoriesFragment();
                 break;
-            case R.id.nav_notification:
-                fragment = new NotificationFragment();
+//            case R.id.nav_notification:
+//                fragment = new NotificationFragment();
+//                break;
+            case R.id.nav_manage_address:
+                fragment = new ManageAddressFragment();
+                break;
+            case R.id.nav_payment_option:
+                fragment = new PaymentOptionsFragment();
+                break;
+            case R.id.nav_refer_and_earn:
+                fragment = new ReferAndEarnFragment();
                 break;
             case R.id.nav_customer_care:
                 fragment = new CustomerCareFragment();
@@ -95,11 +130,23 @@ public class HomeActivity extends AppCompatActivity
             case R.id.nav_faq:
                 fragment = new FAQFragment();
                 break;
-            case R.id.frequently_bought:
-                fragment = new FAQFragment();
+            case R.id.nav_frequently_bought:
+                fragment = new FrequentlyBoughtFragment();
                 break;
-            case R.id.my_store:
-                fragment = new FAQFragment();
+            case R.id.nav_logout:
+                showAlert();
+                break;
+            case R.id.notificationIcon:
+                fragment = new NotificationFragment();
+                break;
+            case R.id.action_my_orders:
+                fragment = new MyOrdersFragment();
+                break;
+            case R.id.action_wishlist:
+                fragment = new WishListFragment();
+                break;
+            case R.id.action_my_store:
+                fragment = new MyStoreFragment();
                 break;
         }
         //replacing the fragment
@@ -111,6 +158,29 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
     }
+
+    private void showAlert() {
+        builder.setMessage("Are you sure? You want to logout?") .setTitle("Logout!!!");
+        builder.setMessage("Do you want to close this application ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        Toast.makeText(getApplicationContext(),"you choose yes action for alertbox",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
